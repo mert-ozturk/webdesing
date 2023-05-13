d04f193d300b8aba40819bf5f85cddabcd7f5d49
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2023-05-13 13:08:10 +0300

    web3

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+
 
 const Write = () => {
+  const [value,setValue] = useState('');
   return (
-    <div className='write'>
-      <div className='content'>content</div>
+    <div className='add'>
+      <div className='content'>
+        <input type='text' placeholder='Title'/>
+        <div className='editorContainer'>
+          <ReactQuill theme='snow' value={value} onChange={setValue} />
+        </div>
+      </div>
       <div className='menu'>
-        <div className='item'>i1</div>
-        <div className='item'>i2</div>
+        <div className='item'>
+          <h1>Publish</h1>
+          <span>
+            <b>Status:</b>Draft
+          </span>
+          <span>
+            <b>Visibility:</b>Public
+          </span>
+          <input style={{display:"none"}} type="file" id="file" name="" />
+          <label htmlFor='file' >Upload Image</label>
+          <div className='buttons'>
+            <button>Save as a draft</button>
+            <button>Update</button>
+          </div>
+        </div>
+        <div className='item'>
+          <h1>Category</h1>
+          <input type='radio' name='cat' value="art" id="art"/>
+          <label htmlFor='art'>Art</label>
+          <input type='radio' name='cat' value="science" id="science"/>
+          <label htmlFor='science'>science</label>
+        
+        </div>
       </div>
       
     </div>
